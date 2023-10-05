1d8a3cab085ca20cfc9fc2c394c07c90a078acb6
aytarakdemir <aytar.akdemir@outlook.com>
2023-10-05 23:25:06 +0300

    Implement vertex moving functionality

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,52 @@
 import p5 from 'p5';
 import { Vertex } from './models/vertex';
 
+
+
+let clickedVertex: Vertex = null;
+
+const inputtedVertices: Vertex[] = [
+    new Vertex(500,390,20,'a'),
+    new Vertex(900,850,15,'b'),
+    new Vertex(700,340,20),
+    new Vertex(90,90,25),
+    new Vertex(150,700,30),
+    new Vertex(120,300,20),
+];
+
+ 
 const sketch = (p: p5) => {
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight - 10);
         p.strokeWeight(2);
+        p.background(220);
+        p.frameRate(60);
     };
     
     p.draw = () => {
-        p.background(220);
-        drawVertices(
-            [
-                new Vertex(500,390,20,'a'),
-                new Vertex(900,850,15,'b'),
-                new Vertex(700,340,20),
-                new Vertex(90,90,25),
-                new Vertex(150,700,30),
-                new Vertex(120,300,20),
-            ], 
-            p
-        );
+        p.clear(0,0,0,0);
+        
+        drawVertices(inputtedVertices, p);
     };
 
-    p.mouseClicked = () => {
-        console.log("asd")
+    p.mousePressed = () => {
+        console.log('pressed')
+        inputtedVertices.forEach(vertex => {
+            if(vertex.isPressed(p.mouseX, p.mouseY)) {
+                clickedVertex = vertex;
+            }            
+        });
     };
+
+    p.mouseReleased = () => {
+        clickedVertex = null;
+    }
+
+    p.mouseDragged = () => {
+        if (clickedVertex) {
+            clickedVertex.moveVertex(p.mouseX, p.mouseY);
+        }
+    }
 };
 
 
@@ -39,10 +61,10 @@ function drawVertices(vertices: Vertex[] = [], p: p5): void {
         }
         
     });
-
-
-
 }
+
+
+
 
 
 new p5(sketch);
--- src/models/vertex.ts
+++ src/models/vertex.ts
@@ -5,10 +5,32 @@ export class Vertex {
     radius: number;
     label: string;
 
+    private xDiff: number;
+    private yDiff: number;
+
     constructor(x = 10, y = 10, radius = 10, label?:string) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         label ? this.label = label: this.label = "";
+    }
+
+    isPressed(mouseX: number, mouseY: number): boolean {
+        const distanceSq: number = (this.x - mouseX) ** 2 + (this.y - mouseY) ** 2;
+        const isInside: boolean = distanceSq < this.radius ** 2;
+        if (isInside) {
+            this.calculateDistanceFromCenter(mouseX, mouseY);    
+        }
+        return isInside;
+    }
+
+    calculateDistanceFromCenter(mouseX: number, mouseY: number): void {
+        this.xDiff = mouseX - this.x;
+        this.yDiff = mouseY - this.y;
+    }
+
+    moveVertex(pressedMouseX: number,pressedMouseY: number): void {
+        this.x = pressedMouseX - this.xDiff;
+        this.y = pressedMouseY - this.yDiff;
     }
 }
